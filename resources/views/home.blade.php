1694bdbaa04c1a939c799250efd8bcb6e8b0611a
ibrahim kedir <techofreact@gmail.com>
2024-10-31 12:54:30 +0300

    ft: basic training.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<x-base>
+    @section('name', 'Ibraheem')
+    <x-slot name="name">Ibraheem</x-slot>
+    <x-slot name="content">
+        <di class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="text-center">Welcome to Laravel Blog</h1>
+                    <p class="text-center">This is a simple blog system built with Laravel</p>
+                </div>
+            </div>
+    </x-slot>
+</x-base>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\welcomeController;
+use App\Models\Student;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [welcomeController::class, 'index']);
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+
+Route::get('/fetchdata', function () {
+    $users = DB::select('select * from users');
+    // dd($users);
+    $users1 = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+    // dd($users1);
+    // using eloquent
+    $students = Student::all();
+    // dd($students);
+    // foreach ($students as $student) {
+    //     echo $student->name . '<br>';
+    // }
+    $students1 = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+    dd($students1);
 });
